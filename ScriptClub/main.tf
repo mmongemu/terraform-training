ec00a4eb94242eda9334d34434611ec8bff836b5
Melissa Monge <mmongemu@akamai.com>
2025-09-26 14:45:22 -0600

    Add datasources

--- ScriptClub/main.tf
+++ ScriptClub/main.tf
@@ -3,23 +3,23 @@ data "akamai_group" "sc_group" {
      contract_id  = "1-5C13O2"
 }
 
-output "sc_group" {
-    value = data.akamai_group.sc_group
+output "sc_group_id" {
+    value = data.akamai_group.sc_group.id
 }
 
 data "akamai_appsec_configuration" "sec_configuration" {
     name = "bucktest"
 }
 
-output "appsec_config" {
-  value = data.akamai_appsec_configuration.sec_configuration
+output "appsec_config_id" {
+  value = data.akamai_appsec_configuration.sec_configuration.id
 }
 
-ata "akamai_property" "mmmongemu_property" {
+data "akamai_property" "mmmongemu_property" {
   name    = "mmongemu-scriptclub"
   version = "7"
 }
 
-output "my_property" {
-  value = data.akamai_property.mmmongemu_property
+output "my_property_id" {
+  value = data.akamai_property.mmmongemu_property.id
 }
